9240157a82aeaa4f11b77b914113575b856490c9
Juan Grasso <juangrassob@gmil.com>
2021-06-29 16:24:57 -0300

    Se agrego la coneccion a la base de datos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+require('dotenv').config();
+
+console.log(process.env.TESTING)
+
 const app = require('./server');
+require('./database');
+
 
 app.listen(app.get('port'), () =>{
     console.log('Server listening at: http://localhost:'+app.get('port'))
