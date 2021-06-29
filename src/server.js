891b29e0eb36a2e68667fdb8c1d14f3914364533
Juan Grasso <juangrassob@gmil.com>
2021-06-29 18:09:33 -0300

    Agregue y configure el TemplateEngine handdlebars

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path')
 
 // Initializations
@@ -7,6 +8,14 @@ const app = express();
 // Settings
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+}));
+
+app.set('view engine', '.hbs');
 
 // Middlewares
 app.use(express.urlencoded({extended: false}));
@@ -16,7 +25,7 @@ app.use(express.urlencoded({extended: false}));
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hola Mundo!')
+    res.render('index');
 });
 
 // Statics Files
