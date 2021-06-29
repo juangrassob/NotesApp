0a977f396f9c1b7ee5ca23a55f2acfaf1f6e2e42
Juan Grasso <juangrassob@gmil.com>
2021-06-29 15:52:25 -0300

    First commit

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path')
+
+// Initializations
+const app = express();
+
+// Settings
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+
+// Middlewares
+app.use(express.urlencoded({extended: false}));
+
+// Global Variables
+
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hola Mundo!')
+});
+
+// Statics Files
+app.use(express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
