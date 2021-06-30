10883bbfd819e1afcda7b658579ac12f42ce059d
Juan Grasso <juangrassob@gmil.com>
2021-06-30 10:53:48 -0300

    Se agrego un router basico

--- src/server.js
+++ src/server.js
@@ -24,9 +24,7 @@ app.use(express.urlencoded({extended: false}));
 
 
 // Routes
-app.get('/', (req, res) => {
-    res.render('index');
-});
+app.use(require('./routes/index.routes'));
 
 // Statics Files
 app.use(express.static(path.join(__dirname, 'public')));
